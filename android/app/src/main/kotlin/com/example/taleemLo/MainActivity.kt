7559e8c5aff3b682cb8bc101c1050c9f5e8c0bb8
MuntaZir Rizvi <50302549+muntazir143@users.noreply.github.com>
2020-08-05 20:54:41 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/taleemLo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.taleemLo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:taleemLo/src/app.dart';
+
+void main() {
+  runApp(App());
+}
+
