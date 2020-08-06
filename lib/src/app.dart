d6d0bbb7df41b31349cfdf878a17de48bef0a25e
MuntaZir Rizvi <50302549+muntazir143@users.noreply.github.com>
2020-08-06 14:37:47 +0500

    Structure enhancement and initial bugs fixes

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:taleemLo/src/screen/home_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('TaleemLo'),
-      ),
-      body: Container(
-        child: Text('TaleemLo')
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TaleemLo',
+      home: HomeScreen(),
     );
   }
 }
